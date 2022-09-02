64a4f8667462717172bfe085cabb1c118f50953f
Vicente Álvarez <v.lvarez.illanes@gmail.com>
2022-09-01 23:11:40 -0400

    proyecto javaee

---
+++ src/main/java/com/giftShopBack/main/GiftShopBackApplication.java
@@ -0,0 +1,13 @@
+package com.giftShopBack.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftShopBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftShopBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giftShopBack/main/GiftShopBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giftShopBack.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftShopBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
