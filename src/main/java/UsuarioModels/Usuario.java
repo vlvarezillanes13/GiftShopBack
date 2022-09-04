297f7de6192ce8c4573e9cbe8d83b97c35443627
Bastipower19 <bastitrabajo2019@gmail.com>
2022-09-04 16:46:57 -0300

    cambio a modo entity del modelo

--- src/main/java/UsuarioModels/Usuario.java
+++ src/main/java/UsuarioModels/Usuario.java
@@ -1,4 +1,11 @@
-package UsuarioModels;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity//This tells Hibernate to make a table out of this class User{
+@Id
+@GeneratedValue(strategy=GenerationType.AUTO)
 
 	public class Usuario {
 		
@@ -10,26 +17,44 @@ package UsuarioModels;
 		private String Estado;
 		
 		public Usuario() {
-			System.out.println(IdUsuario+ "" + Nombre + "" + Apellido + "" + Username +"" + Clave + "" + Estado);
 			
 		}
 		public Integer getIdUsuario() {
 			return IdUsuario;
 		}
+		public void setIdUsuario(Integer IdUsuario) {
+			this.IdUsuario = IdUsuario;
+		}
 		public String getNombre() {
 			return Nombre;
+		}
+		public void setNombre(String Nombre) {
+			this.Nombre = Nombre;
 		}
 		public String getApellido() {
 			return Apellido;
 		}
+		public void setApellido(String Apellido) {
+			this.Apellido = Apellido;
+		}
 		public String getUsername() {
 			return Username;
+		}
+		public void setUsername(String Username) {
+			this.Username = Username;
 		}
 		public Integer getClave() {
 			return Clave;
 		}
+		public void setClave (Integer Clave) {
+			this.Clave = Clave;
+		}
 		public String getEstado() {
 			return Estado;
 		}
+		public void setEstado(String Estado) {
+			this.Estado = Estado;
+		}
 	}
+
 
